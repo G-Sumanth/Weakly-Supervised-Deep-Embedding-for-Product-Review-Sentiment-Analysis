1f9434399465dbe8336290ce5727bb1a0490c0b1
Gangeri Sumanth Kumar <85611185+G-Sumanth@users.noreply.github.com>
2021-06-11 10:16:52 +0530

    Update forms.py

--- admins/forms.py
+++ admins/forms.py
@@ -7,11 +7,7 @@ class ProductForm(forms.ModelForm):
     name = forms.CharField(widget=TextInput(attrs={'class':'userna common','placeholder': 'Name'}))
     weight = forms.CharField(widget=TextInput(attrs={'class':'userna common','placeholder': 'Weights'}))
     color = forms.ChoiceField(widget=Select(attrs={'class':'userna common'}),choices=CATEGORY_COLORS)
-    price = forms.CharField(widget=NumberInput(attrs={'class':'passwo common','placeholder':'Price'}))
-    category = forms.ChoiceField(widget=Select(attrs={'class':'passwo common'}),choices=CATEGORY_CHOICES)
-    stock = forms.CharField(widget=NumberInput(attrs={'class':'email common','placeholder':'Stock'}))
-    description = forms.CharField(widget=TextInput(attrs={'class':'email common','placeholder':'Description'}))
-    images = forms.FileField(widget=FileInput(attrs={'class':'email common'}))
+   
     class Meta:
         model = Products
         fields = ('name', 'weight', 'color', 'price', 'category', 'stock', 'description', 'images',)
